02ea0223e168adc036e93d344d038a87514127b4
micovihavainen <mico.vihavainen@outlook.com>
2020-11-26 17:02:46 +0200

    Progressing on the game...

--- po.unity.project1/Assets/CameraController.cs
+++ po.unity.project1/Assets/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     public float offset;
+    public float offset_y;
     private Vector3 playerPosition;
     public float offsetSmoothing;
     // Use this for initialization
@@ -17,14 +18,14 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
         if (player.transform.localScale.x > 0f)
         {
-            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y + offset_y, playerPosition.z);
         }
         else
         {
-            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y +offset_y, playerPosition.z);
         }
         transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
     }
--- po.unity.project1/Assets/PlayerController.cs
+++ po.unity.project1/Assets/PlayerController.cs
@@ -4,14 +4,24 @@ using System.IO.MemoryMappedFiles;
 using System.Runtime.InteropServices.ComTypes;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D PlayerRb;
     private Animator PlayerAnim;
-    public float speed = 20;
-    public float jumpforce = 5;
+    public float speed = 10;
+    public float jumpforce = 12;
     private bool isGrounded = true;
+
+    public Transform explosion;
+
+    public int health = 5;
+    public TextMeshProUGUI healthtext;
+
+    public TextMeshProUGUI cherrytext;
+    public int cherries = 0;
     void Start()
     {
         PlayerRb = GetComponent<Rigidbody2D>();
@@ -32,7 +42,7 @@ public class PlayerController : MonoBehaviour
         {
             PlayerAnim.SetBool("Running", false);
         }
-        
+
         // Turning when going to the opposite direction
         if (HorizontalInput > 0)
         {
@@ -49,16 +59,73 @@ public class PlayerController : MonoBehaviour
             PlayerRb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
             PlayerAnim.SetBool("Jumping", true);
             isGrounded = false;
+            explosion.GetComponent<ParticleSystem>().Play(explosion);
+        }
+
+        // Ground check fix
+        if (Input.GetKeyDown(KeyCode.S) && isGrounded == false)
+        {
+            isGrounded = true;
+            PlayerAnim.SetBool("Jumping", false);
         }
     }
 
-    // Ground check
+    // Collision for collectibles
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Destroy collectible when collided with and update text
+        if (collision.tag == "Collectible")
+        {
+            Destroy(collision.gameObject);
+            cherries += 1;
+            cherrytext.text = cherries.ToString();
+        }
+    }
+    // Collision mechanics
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // Ground check / disabling hurt and enabling movement when landing
         if (collision.gameObject.CompareTag("Platform"))
         {
             isGrounded = true;
             PlayerAnim.SetBool("Jumping", false);
+            speed = 10;
+            PlayerAnim.SetBool("Hurting", false);
+        }
+
+        // Enemy behaviour
+
+        // Enemy dies when player is jumping and colliding
+        if (collision.gameObject.CompareTag("Enemy") && isGrounded == false)
+        {
+            Destroy(collision.gameObject);
+            PlayerRb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+        }
+
+        // When player is moving from the right, player gets thrown left and up, while playing "hurt" animation
+        // Player speed is disabled and isGrounded = false is preventing the player from jumping
+        else if (collision.gameObject.CompareTag("Enemy") && Input.GetAxis("Horizontal") > 0)
+        {
+            PlayerRb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+            PlayerRb.AddForce(Vector2.left * jumpforce, ForceMode2D.Impulse);
+            PlayerAnim.SetBool("Hurting", true);
+            speed = 0;
+            isGrounded = false;
+            health--;
+            healthtext.text = health.ToString();
+        }
+
+        // When player is moving from the left, player gets thrown right and up, while playing "hurt" animation
+        // Player speed is disabled and isGrounded = false is preventing the player from jumping
+        else if (collision.gameObject.CompareTag("Enemy") && Input.GetAxis("Horizontal") < 0)
+        {
+            PlayerRb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+            PlayerRb.AddForce(Vector2.right * jumpforce, ForceMode2D.Impulse);
+            PlayerAnim.SetBool("Hurting", true);
+            speed = 0;
+            isGrounded = false;
+            health--;
+            healthtext.text = health.ToString();
         }
     }
 }
